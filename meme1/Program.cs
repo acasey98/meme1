a8db25f58409521a4688ba0c56688918dd0cb7a1
Austin Casey <thepoast@gmail.com>
2019-08-20 20:30:00 -0500

    xd

---
+++ meme1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace meme1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var decimalVal = 12.123m;
+
+            DateTime myDateTime = new DateTime();
+
+
+     
+        }
+    }
+}
